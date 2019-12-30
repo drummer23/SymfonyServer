ce255081c0597d7d58bbba730f78ad9e974b9db9
Roger Spielmann <drummer23@braveheart.ch>
2019-12-30 15:16:12 +0700

    adds files that consists a running exapmple

---
+++ src/Controller/PersonController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Person;
+
+class PersonController extends AbstractController
+{
+    /**
+     * @Route("/person", name="person")
+     */
+    public function index()
+    {
+        $persons = $this->getDoctrine()
+        ->getRepository(Person::class)
+        ->findAll();
+
+        if (!$persons) {
+          throw $this->createNotFoundException(
+            'No persons found'
+          );
+        }
+
+        $names = array();
+
+
+        /* @var $person Person */
+        foreach ($persons as $person) {
+            $names[] = $person->getName();
+
+            // code...
+        }
+
+        return $this->render('person/index.html.twig', [
+            'controller_name' => 'PersonController',
+            'persons' => implode('-', $names)
+        ]);
+    }
+}
